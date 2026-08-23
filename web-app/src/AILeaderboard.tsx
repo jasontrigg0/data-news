54f909c5e1d124a7ec08942f841f437207e430a8
Jason Trigg <jasontrigg0@gmail.com>
2026-08-23 17:30:45 -0400

    Add 7/1/26 to the pricing filter

--- web-app/src/AILeaderboard.tsx
+++ web-app/src/AILeaderboard.tsx
@@ -107,6 +107,7 @@ const PerformanceChart = ({xAxisVar, xAxisTicks, xAxisDomain, xAxisLabel}) => {
     {id: "20250101", label: "Jan 1, 2025"},
     {id: "20250701", label: "Jul 1, 2025"},
     {id: "20260101", label: "Jan 1, 2026"},
+    {id: "20260701", label: "Jul 1, 2026"},
     {id: null, label: "Now"},
   ];
 
